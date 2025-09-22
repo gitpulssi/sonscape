88b363456b17deace82d277c0930784af5bea7f0
gitpulssi <152341354+gitpulssi@users.noreply.github.com>
2025-09-22 12:41:41 -0700

    Update install_sonixscape.sh

--- install_sonixscape.sh
+++ install_sonixscape.sh
@@ -27,7 +27,7 @@ sudo apt-get -y upgrade
 info "Installing dependencies..."
 sudo apt-get install -y \
     python3 python3-pip python3-venv \
-    python3-numpy python3-scipy python3-flask \
+    python3-numpy python3-flask \
     python3-websockets python3-alsaaudio \
     alsa-utils git curl \
     bluetooth bluez
